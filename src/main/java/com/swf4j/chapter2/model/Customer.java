21455ca1aa36385bedbc26c10ead9297612b7311
uzig305 <zjt_james@163.com>
2019-04-10 23:02:48 +0800

    init

---
+++ src/main/java/com/swf4j/chapter2/model/Customer.java
@@ -0,0 +1,66 @@
+/**
+ * created by Zheng Jiateng on 2019/4/10.
+ */
+package com.swf4j.chapter2.model;
+
+/**
+ * 客户
+ */
+public class Customer {
+    private long id;
+    private String name;
+    // 联系人
+    private String contact;
+    private String telephone;
+    private String email;
+    // 备注
+    private String remark;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+}
