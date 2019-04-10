a2a6b08fe527d4d4bb8f617e15ef5968257f8865
uzig305 <zjt_james@163.com>
2019-04-10 23:29:46 +0800

    服务层

---
+++ src/main/java/com/swf4j/chapter2/service/CustomerService.java
@@ -0,0 +1,54 @@
+/**
+ * created by Zheng Jiateng on 2019/4/10.
+ */
+package com.swf4j.chapter2.service;
+
+import com.swf4j.chapter2.model.Customer;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 服务层 业务逻辑
+ */
+public class CustomerService {
+    /**
+     * 获取客户列表
+     */
+    public List<Customer> getCustomerList(String keyword) {
+        //todo
+        return null;
+    }
+
+    /**
+     * 获取客户
+     */
+    public Customer getCustomer(long id) {
+        //todo
+        return null;
+    }
+
+    /**
+     * 创建客户
+     */
+    public boolean createCustomer(Map<String, Object> fieldMap) {
+        //todo
+        return false;
+    }
+
+    /**
+     * 更新客户
+     */
+    public boolean updateCustomer(long id, Map<String, Object> fieldMap) {
+        //todo
+        return false;
+    }
+
+    /**
+     * 删除客户
+     */
+    public boolean deleteCustomer(long id) {
+        //todo
+        return false;
+    }
+}
