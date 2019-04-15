c588327b56faac3c7c0f8d179a48e5200e6678ec
uzig305 <zjt_james@163.com>
2019-04-15 22:05:02 +0800

    queryEntityList & queryEntity

--- src/main/java/com/swf4j/chapter2/helper/DatabaseHelper.java
+++ src/main/java/com/swf4j/chapter2/helper/DatabaseHelper.java
@@ -6,11 +6,15 @@ package com.swf4j.chapter2.helper;
 import com.swf4j.chapter2.util.PropertiesUtil;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 数据库操作助手类
@@ -19,8 +23,10 @@ public final class DatabaseHelper {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DatabaseHelper.class);
 
+    // 用ThreadLocal存放当前线程中的Connection 可以将ThreadLocal理解为一个隔离线程的容器
     private static final ThreadLocal<Connection> CONNECTION_HOLDER;
 
+    // 使用DbUtils提供的QueryRunner对象可以面向实体（Entity）进行查询
     private static final QueryRunner QUERY_RUNNER;
 
     // 使用Apache DBCP的BasicDataSource来获取数据库连接 保证对象是静态的
@@ -62,6 +68,37 @@ public final class DatabaseHelper {
         return conn;
     }
 
+    /**
+     * 查询实体列表
+     */
+    public static <T> List<T> queryEntityList(Class<T> entityClass, String sql, Object... params) {
+        List<T> entityList;
+        try {
+            Connection conn = getConnection();
+            // DbUtil首先执行sql语句并返回一个ResultSet，随后由ResultSetHandler将ResultSet转换为所需类型对象，
+            // 因此要选合适的ResultSetHandler（此例中是BeanListHandler）
+            // new泛型类的时候必须要加<>  new BeanListHandler<T>(entityClass)
+            entityList = QUERY_RUNNER.query(conn, sql, new BeanListHandler<T>(entityClass), params);
+        } catch (SQLException e) {
+            LOGGER.error("query entity list failure", e);
+            throw new RuntimeException(e);
+        } // 用了DBCP连接池之后就不需要再finally closeConnection();
+        return entityList;
+    }
 
+    /**
+     * 查询单个实体
+     */
+    public static <T> T queryEntity(Class<T> entityClass, String sql, Object... params) {
+        T entity;
+        try {
+            Connection conn = getConnection();
+            entity = QUERY_RUNNER.query(conn, sql, new BeanHandler<T>(entityClass), params);
+        } catch (SQLException e) {
+            LOGGER.error("query entity failure", e);
+            throw new RuntimeException(e);
+        }
+        return entity;
+    }
 
 }
