e627a76b2b6de49727bc7007e4a7bb92cb8ee282
uzig305 <zjt_james@163.com>
2019-04-16 23:06:10 +0800

    Service

--- src/main/java/com/swf4j/chapter2/service/CustomerService.java
+++ src/main/java/com/swf4j/chapter2/service/CustomerService.java
@@ -3,6 +3,7 @@
  */
 package com.swf4j.chapter2.service;
 
+import com.swf4j.chapter2.helper.DatabaseHelper;
 import com.swf4j.chapter2.model.Customer;
 import com.swf4j.chapter2.util.PropertiesUtil;
 
@@ -14,46 +15,43 @@ import java.util.Map;
  */
 public class CustomerService {
 
-
-
     /**
      * 获取客户列表
+     *
      */
+
+
     public List<Customer> getCustomerList() {
-        "".getClass();
-        //todo
-        return null;
+        String sql = "SELECT * FROM customer";
+        return DatabaseHelper.queryEntityList(Customer.class, sql);
     }
 
     /**
      * 获取客户
      */
     public Customer getCustomer(long id) {
-        //todo
-        return null;
+        String sql = "SELECT * FROM customer WHERE id = ?";
+        return DatabaseHelper.queryEntity(Customer.class, sql, id);
     }
 
     /**
      * 创建客户
      */
     public boolean createCustomer(Map<String, Object> fieldMap) {
-        //todo
-        return false;
+        return DatabaseHelper.insertEntity(Customer.class, fieldMap);
     }
 
     /**
      * 更新客户
      */
     public boolean updateCustomer(long id, Map<String, Object> fieldMap) {
-        //todo
-        return false;
+        return DatabaseHelper.updateEntity(Customer.class, id, fieldMap);
     }
 
     /**
      * 删除客户
      */
     public boolean deleteCustomer(long id) {
-        //todo
-        return false;
+        return DatabaseHelper.deleteEntity(Customer.class, id);
     }
 }
